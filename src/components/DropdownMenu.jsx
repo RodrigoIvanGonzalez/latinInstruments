13508bf0cd26580f12d3afd513f8bd8d05e4068c
RodrigoIvanGonzalez <r.ivann.gonzalezz@gmail.com>
2024-11-28 14:51:38 -0300

    fix arreglo y rediseno de dropdownmenu utilizando framermotion

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,185 +1,158 @@
+import { useState } from "react";
+import  { AnimatePresence, motion } from 'framer-motion';
+import { productsData } from './Data'
 
-
-const DropdownMenu = () => {
-return (
-<nav className="">
-<section className="relative">
-  <ul className="list-none m-0 p-0 bg-teal-600 flex justify-center gap-2 w-full">
-  <li className="relative float-left group">
-      <a className="bg-teal-600 text-white block font-normal text-sm py-3 px-14 text-center transition-all duration-250 ease-in-out hover:bg-gray-700" href="">
+const Dropdownmenu = () => {
+  return(
+    <div className="flex  justify-around  bg-neutral-900 px-3 py-3 mb-20 xs:hidden ">
+      <FlyoutLink href="#" FlyoutContent = {GuitarContent}>
         Guitarras
-      </a>
-      <ul className="absolute left-0 top-9 opacity-0 invisible z-10 transition-all duration-250 ease-in-out group-hover:opacity-100 group-hover:visible group-hover:top-20">
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-      </ul>
-    </li>
-
-    <li className="relative float-left group">
-      <a className="bg-teal-600 text-white block font-normal text-sm py-3 px-14 text-center transition-all duration-250 ease-in-out hover:bg-gray-700" href="">
+      </FlyoutLink>
+      <FlyoutLink href="#" FlyoutContent = {BassContent}>
         Bajos
-      </a>
-      <ul className="absolute left-0 top-9 opacity-0 invisible z-10 transition-all duration-250 ease-in-out group-hover:opacity-100 group-hover:visible group-hover:top-20">
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-      </ul>
-    </li>
-
-    <li className="relative float-left group">
-      <a className="bg-teal-600 text-white block font-normal text-sm py-3 px-14 text-center transition-all duration-250 ease-in-out hover:bg-gray-700" href="">
+      </FlyoutLink>
+      <FlyoutLink href="#" FlyoutContent = {DrumsContent}>
         Baterias
-      </a>
-      <ul className="absolute left-0 top-9 opacity-0 invisible z-10 transition-all duration-250 ease-in-out group-hover:opacity-100 group-hover:visible group-hover:top-20">
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-      </ul>
-    </li>
-
-    <li className="relative float-left group">
-      <a className="bg-teal-600 text-white block font-normal text-sm py-3 px-14 text-center transition-all duration-250 ease-in-out hover:bg-gray-700" href="">
+      </FlyoutLink>
+      <FlyoutLink href="#" FlyoutContent = {KeysContent}>
         Teclados
-      </a>
-      <ul className="absolute left-0 top-9 opacity-0 invisible z-10 transition-all duration-250 ease-in-out group-hover:opacity-100 group-hover:visible group-hover:top-20">
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-      </ul>
-    </li>
-
-    <li className="relative float-left group">
-      <a className="bg-teal-600 text-white block font-normal text-sm py-3 px-14 text-center transition-all duration-250 ease-in-out hover:bg-gray-700" href="">
+      </FlyoutLink>
+      <FlyoutLink href="#" FlyoutContent = {SoundContent}>
         Sonido
-      </a>
-      <ul className="absolute left-0 top-9 opacity-0 invisible z-10 transition-all duration-250 ease-in-out group-hover:opacity-100 group-hover:visible group-hover:top-20">
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-      </ul>
-    </li>
-
-    <li className="relative float-left group">
-      <a className="bg-teal-600 text-white block font-normal text-sm py-3 px-14 text-center transition-all duration-250 ease-in-out hover:bg-gray-700" href="">
+      </FlyoutLink>
+      <FlyoutLink href="#" FlyoutContent = {AccesoriesContent}>
         Accesorios
-      </a>
-      <ul className="absolute left-0 top-9 opacity-0 invisible z-10 transition-all duration-250 ease-in-out group-hover:opacity-100 group-hover:visible group-hover:top-20">
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-      </ul>
-    </li>
+      </FlyoutLink>
+    </div>
 
-    <li className="relative float-left group">
-      <a className="bg-teal-600 text-white block font-normal text-sm py-3 px-14 text-center transition-all duration-250 ease-in-out hover:bg-gray-700" href="">
-        Marcas
-      </a>
-      <ul className="absolute left-0 top-9 opacity-0 invisible z-10 transition-all duration-250 ease-in-out group-hover:opacity-100 group-hover:visible group-hover:top-20">
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-        <li className="w-full">
-          <a className="block bg-gray-700 text-white py-3 px-14 text-center hover:bg-orange-600" href="">
-            asd
-          </a>
-        </li>
-      </ul>
-    </li>
-  </ul>
-</section>
-</nav>
 )
 }
 
+const FlyoutLink = ({ children, href, FlyoutContent }) => {
+  const [open, setOpen] = useState(false);
+  const showFlyout = open && FlyoutContent;
+
+  return(
+    <div 
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      className=" relative h-fit w-fit "> 
+      <a href={href} className=" relative text-white "> { children }
+        <span 
+        style={{ 
+          transform: showFlyout ? 'scaleX(1)' : 'scaleX(0)',
+          }}
+        className=" absolute -bottom-2 -left-2 -right-2 h-1 origin-left rounded-full bg-indigo-300 transition-transform duration-300 ease-out " />
+      </a>
+      <AnimatePresence>
+      {showFlyout && ( 
+        <motion.div
+        initial={{ opacity: 0, y: 15 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 15 }}
+        style={{ x: '-50%' }}
+        transition={{ duration: 0.3, ease: 'easeOut' }}
+        className=" absolute left-1/2 top-12 bg-burgundy-dark text-black ">
+
+          <div className=" absolute -top-6 left-0 right-0 h-6 bg-transparent"/>
+          <div className=" absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white  "/>
+          <FlyoutContent />
+        </motion.div> ) }
+        </AnimatePresence>
+    </div>
+  )
 
 
+}
 
+const GuitarContent = () =>{ 
 
+  const productGuitar = productsData.filter( guitar => guitar.type === "Guitarra" )
 
+  return(
+    <div className=" w-64  bg-white p-6 shadow-xl ">
+      {productGuitar.map((product, i) => (
+        <div key={i} className="mb-6 space-y-3">
+          <h3><a href="">{product.brand}</a></h3>
+        </div>
+      ))}
+    </div>
+  )
+}
 
+const BassContent = () =>{ 
 
+  const productBass = productsData.filter( bass => bass.type === "Bajo" )
 
+  return(
+    <div className=" w-64  bg-white p-6 shadow-xl ">
+      {productBass.map((product, i) => (
+        <div key={i} className="mb-6 space-y-3">
+          <h3><a href="">{product.brand}</a></h3>
+        </div>
+      ))}
+    </div>
+  )
+}
 
+const DrumsContent = () =>{ 
 
+  const productDrums = productsData.filter( drums => drums.type === "Bateria" )
 
-export default DropdownMenu
+  return(
+    <div className=" w-64  bg-white p-6 shadow-xl ">
+      {productDrums.map((product, i) => (
+        <div key={i} className="mb-6 space-y-3">
+          <h3><a href="">{product.brand}</a></h3>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const KeysContent = () =>{ 
+
+  const productKeys = productsData.filter( key => key.type === "Teclado" )
+
+  return(
+    <div className=" w-64  bg-white p-6 shadow-xl ">
+      {productKeys.map((product, i) => (
+        <div key={i} className="mb-6 space-y-3">
+          <h3><a href="">{product.brand}</a></h3>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const AccesoriesContent = () =>{ 
+
+  const productAccesories = productsData.filter( Accesory => Accesory.type === "Accesorios" )
+
+  return(
+    <div className=" w-64  bg-white p-6 shadow-xl ">
+      {productAccesories.map((product, i) => (
+        <div key={i} className="mb-6 space-y-3">
+          <h3><a href="">{product.brand}</a></h3>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const SoundContent = () =>{ 
+
+  const productSound = productsData.filter( sound => sound.type === "Sonido" )
+
+  return(
+    <div className=" w-64  bg-white p-6 shadow-xl ">
+      {productSound.map((product, i) => (
+        <div key={i} className="mb-6 space-y-3">
+          <h3><a href="">{product.brand}</a></h3>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Dropdownmenu
